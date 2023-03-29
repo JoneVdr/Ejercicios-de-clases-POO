0fa6666360fa3935e2f115de4152b6be1fd5e1ad
BobiStudio <mgonzgarc@myuax.com>
2023-03-30 00:38:56 +0200

    ejercicioA

--- ejercicioA/ejercicioA.py
+++ ejercicioA/ejercicioA.py
@@ -16,22 +16,34 @@ print(Palindromo.esPalindromo('L O L'))
 >>> True """
 
 
-#Codigo:
-
 class Palindromo:
     def __init__(self, palabra):
-        self.palabra = palabra
+        self.palabra = str(palabra)
 
-    @classmethod
-    def esPalindromo(cls):
-        palabra = palabra.replace(' ', '').lower()
+    @classmethod #Decorador classmethod porque no usa ninguna propiedad de instancia de Palindromo, sino que solo se basa en el argumento que se pasa como entrada.
+    def esPalindromo(cls, palabra):
+        palabra = palabra.lower()
         if palabra == palabra[::-1]:
             return True
         else:
             return False
-        print(Palindromo.esPalindromo('radar'))
-        print(Palindromo.esPalindromo('sonar'))
-        print(Palindromo.esPalindromo('Arde ya la yedra'))
-        print(Palindromo.esPalindromo('Ardeyalayedra'))
-        print(Palidromo.esPalindromo('!@#$% %$#@!'))
-        print(Palindromo.esPalindromo('L O L'))
+
+
+if __name__ == '__main__':
+    print(Palindromo.esPalindromo('radar'))
+    print(Palindromo.esPalindromo('sonar'))
+    print(Palindromo.esPalindromo('Arde ya la yedra'))
+    print(Palindromo.esPalindromo('Ardeyalayedra'))
+    print(Palindromo.esPalindromo('!@#$% %$#@!'))
+    print(Palindromo.esPalindromo('L O L'))
+
+
+'''
+DEVUELVE:
+>>> True
+>>> False
+>>> False
+>>> True
+>>> True
+>>> True
+'''
