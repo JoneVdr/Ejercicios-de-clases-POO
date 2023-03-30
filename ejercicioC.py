fddfa76f3420c1739239107543c46f037e033330
jone <jvegadel@myuax.com>
2023-03-30 11:01:06 +0200

    .

--- ejercicioC.py
+++ ejercicioC.py
@@ -1,19 +1,36 @@
 """Enunciado: adivina qué mensajes se muestran mediante el siguiente código voluntariamente poco comprensible:
 
-class A: 
-    def z(self): 
-        return self 
- 
-    def y(self, t): 
-        return len(t) 
- 
-a = A 
-y = a.z 
-print(y(a)) 
-aa = a() 
-print(aa is a()) 
-z = aa.y 
-print(z(())) 
-print(a().y((a,))) 
-print(A.y(aa, (a,z))) 
+class A:
+    def z(self):
+        return self
+
+    def y(self, t):
+        return len(t)
+
+a = A
+y = a.z
+print(y(a))
+aa = a()
+print(aa is a())
+z = aa.y
+print(z(()))
+print(a().y((a,)))
+print(A.y(aa, (a,z)))
 print(aa.y((z,1,'z'))) """
+class A:
+    def z(self):
+        return self
+
+    def y(self, t):
+        return len(t)
+
+a = A
+y = a.z
+print(y(a))
+aa = a()
+print(aa is a())
+z = aa.y
+print(z(()))
+print(a().y((a,)))
+print(A.y(aa, (a,z)))
+print(aa.y((z,1,'z')))
