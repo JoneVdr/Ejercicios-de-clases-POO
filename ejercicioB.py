8772650e6404b1f8ab6018128977be7715c07245
BobiStudio <mgonzgarc@myuax.com>
2023-03-30 10:46:10 +0200

    ejercicioB

--- ejercicioB.py
+++ ejercicioB.py
@@ -14,3 +14,23 @@ Pregunta adicional: ¿por qué se muestra RADAR después de la instanciación Pa
 from ejercicioA import Palindromo
 class Palindromo(Palindromo):
     def __init__(self, palabra):
+        super().__init__(palabra)
+    def test(self):
+        if self.esPalindromo(self.palabra):
+            return True
+        else:
+            return False
+    def __del__(self): #Destructor
+        print(self.palabra.upper())
+
+
+p = Palindromo("radar")
+print(p.test())
+p = Palindromo("sonar")
+print(p.test())
+
+
+'''
+Despues de la instanción Palindromo ("sonar") se muestra RADAR porque el método destructor
+imprime la palabra en mayúsculas, y la palabra es "radar" porque es la última palabra que se
+instanció. Al ejecutar p = Palindromo("radar") se ejecuta del"'''
